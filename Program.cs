36dcc3f93dd0b565557a2aa10086621ef935e6f7
Aniruddha Chatterjee <ruddha.mine@gmail.com>
2021-01-11 20:30:26 +0530

    feat(*): add hello world codes

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstDotNetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Local Hack Day!");
+            Console.WriteLine("This is Sudoers Guild.");
+            string name = "Aniruddha";
+            Console.WriteLine($"I am {name}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Hello World with C#
+
+This is a very simple application, written in C#.
+It deals with simple variable declaration and printing.
+
+### Inspiration
+
+This project is submitted as a part of MLH Local Hack Day 1 - Hello World in a new language
